ba2e783817d9022cc9272d8c21e35fac24ad706a
joaobnv <breno9714@gmail.com>
2024-09-07 21:29:50 -0300

    SQLite: create the token package
    Create the structure for a token. Must not store the lexeme
    for key words.
    
    Create the type for the types of tokens. Use the name "Kind" instead of
    "Type" because type is a reserved word in Go so "Kind" is less likely to
    cause problems with the language.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module mel
+
+go 1.23.0
---
+++ sqlite/v3_46_1/token/token.go
@@ -0,0 +1,164 @@
+// package token deals with tokens.
+package token
+
+// Kind is the type of a token.
+type Kind int
+
+const (
+	// key words
+	KindAbort Kind = iota
+	KindAction
+	KindAdd
+	KindAfter
+	KindAll
+	KindAlter
+	KindAlways
+	KindAnalyze
+	KindAnd
+	KindAs
+	KindAsc
+	KindAttach
+	KindAutoincrement
+	KindBefore
+	KindBegin
+	KindBetween
+	KindBy
+	KindCascade
+	KindCase
+	KindCast
+	KindCheck
+	KindCollate
+	KindColumn
+	KindCommit
+	KindConflict
+	KindConstraint
+	KindCreate
+	KindCross
+	KindCurrent
+	KindCurrentdate
+	KindCurrenttime
+	KindCurrenttimestamp
+	KindDatabase
+	KindDefault
+	KindDeferrable
+	KindDeferred
+	KindDelete
+	KindDesc
+	KindDetach
+	KindDistinct
+	KindDo
+	KindDrop
+	KindEach
+	KindElse
+	KindEnd
+	KindEscape
+	KindExcept
+	KindExclude
+	KindExclusive
+	KindExists
+	KindExplain
+	KindFail
+	KindFilter
+	KindFirst
+	KindFollowing
+	KindFor
+	KindForeign
+	KindFrom
+	KindFull
+	KindGenerated
+	KindGlob
+	KindGroup
+	KindGroups
+	KindHaving
+	KindIf
+	KindIgnore
+	KindImmediate
+	KindIn
+	KindIndex
+	KindIndexed
+	KindInitially
+	KindInner
+	KindInsert
+	KindInstead
+	KindIntersect
+	KindInto
+	KindIs
+	KindIsnull
+	KindJoin
+	KindKey
+	KindLast
+	KindLeft
+	KindLike
+	KindLimit
+	KindMatch
+	KindMaterialized
+	KindNatural
+	KindNo
+	KindNot
+	KindNothing
+	KindNotnull
+	KindNull
+	KindNulls
+	KindOf
+	KindOffset
+	KindOn
+	KindOr
+	KindOrder
+	KindOthers
+	KindOuter
+	KindOver
+	KindPartition
+	KindPlan
+	KindPragma
+	KindPreceding
+	KindPrimary
+	KindQuery
+	KindRaise
+	KindRange
+	KindRecursive
+	KindReferences
+	KindRegexp
+	KindReindex
+	KindRelease
+	KindRename
+	KindReplace
+	KindRestrict
+	KindReturning
+	KindRight
+	KindRollback
+	KindRow
+	KindRows
+	KindSavepoint
+	KindSelect
+	KindSet
+	KindTable
+	KindTemp
+	KindTemporary
+	KindThen
+	KindTies
+	KindTo
+	KindTransaction
+	KindTrigger
+	KindUnbounded
+	KindUnion
+	KindUnique
+	KindUpdate
+	KindUsing
+	KindVacuum
+	KindValues
+	KindView
+	KindVirtual
+	KindWhen
+	KindWhere
+	KindWindow
+	KindWith
+	KindWithout
+)
+
+// Token is a token from the code.
+type Token struct {
+	// Lexeme is the lexeme of the token.
+	Lexeme string
+	// Kind is the kind of the token.
+	Kind Kind
+}
