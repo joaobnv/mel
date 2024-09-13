b41dae8fc2c966f0ae2b10e2595467c71a974d01
joaobnv <breno9714@gmail.com>
2024-09-13 13:32:35 -0300

    SQLite: create the tests for Token

--- sqlite/v3_46_1/token/token_test.go
+++ sqlite/v3_46_1/token/token_test.go
@@ -2,7 +2,7 @@ package token
 
 import "testing"
 
-// TestKind tests the Kind's MarshallText method.
+// TestKindMarshall tests the Kind's MarshallText method.
 func TestKindMarshall(t *testing.T) {
 	var k Kind
 	k = KindTable
@@ -24,7 +24,7 @@ func TestKindMarshall(t *testing.T) {
 	}
 }
 
-// TestKind tests the Kind's UnmarshallText method.
+// TestKindUnmarshall tests the Kind's UnmarshallText method.
 func TestKindUnmarshall(t *testing.T) {
 	var k Kind
 	err := k.UnmarshalText([]byte("Select"))
@@ -52,5 +52,62 @@ func TestKindUnmarshall(t *testing.T) {
 
 	if k != 1000 {
 		t.Errorf("expected 1000, got %s", k.String())
+	}
+
+	err = k.UnmarshalText([]byte("foo"))
+	if err == nil {
+		t.Errorf("error expected")
+	}
+}
+
+// TestTokenMarshall tests the token's MarshallText method.
+func TestTokenMarshall(t *testing.T) {
+	tok := New([]byte("table"), KindTable)
+	data, _ := tok.MarshalText()
+	if string(data) != `<"table", Table>` {
+		t.Errorf(`expected <"table", Table>, got %s`, string(data))
+	}
+}
+
+// TestTokenUnmarshall tests the token's UnmarshallText method.
+func TestTokenUnmarshall(t *testing.T) {
+	var tok Token
+	err := tok.UnmarshalText([]byte(`<"select", Select>`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if tok.Kind != KindSelect {
+		t.Errorf("expected KindSelect, got Kind%s", tok.Kind.String())
+	}
+
+	if string(tok.Lexeme) != "select" {
+		t.Errorf("expected select, got %s", string(tok.Lexeme))
+	}
+
+	if err = tok.UnmarshalText([]byte(`<"\"select\"", Identifier>`)); err != nil {
+		t.Fatal(err)
+	}
+
+	if tok.Kind != KindIdentifier {
+		t.Errorf("expected KindIdentifier, got Kind%s", tok.Kind.String())
+	}
+
+	if string(tok.Lexeme) != "\"select\"" {
+		t.Errorf("expected \"select\", got %s", string(tok.Lexeme))
+	}
+
+	if err = tok.UnmarshalText([]byte(`invalid encoding`)); err == nil {
+		t.Error("expected error, got nil")
+	} else if err.Error() != "invalid token text encoding" {
+		t.Errorf("expected \"invalid token text encoding\", got \"%s\"", err)
+	}
+
+	if err = tok.UnmarshalText([]byte(`<"\xselect", Identifier>`)); err == nil {
+		t.Error("expected error, got nil")
+	}
+
+	if err = tok.UnmarshalText([]byte(`<"select", foo>`)); err == nil {
+		t.Error("expected error, got nil")
 	}
 }
